b6611bd7ba09cd04e8cafcd547ff1554a1e61e2a
Noelle Mittelstaedt <noellemittelstaedt@gmail.com>
2022-03-04 16:05:25 -0500

    cat

--- bash.js
+++ bash.js
@@ -1,15 +1,26 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require ('./cat');
 
 // Output a prompt
 process.stdout.write('prompt > ');
 //stdin
 process.stdin.on('data', (data) => {
 	let cmd = data.toString().trim();
+
+
 	if (cmd === 'pwd') {
 		pwd(cmd);
 	}
 	if (cmd === 'ls') {
 		ls(cmd);
 	}
-});
+	if (cmd.split(' ')[0] === 'cat') {
+		let inputs = cmd.split(' ');
+		// console.log(inputs);
+		// if (inputs[0] === 'cat'){
+			console.log(cmd);
+			cat(inputs[1]);
+		}
+	}
+);
