3c23ab146ccf482696e308eabc35113ee8fb7053
Songyee Han <hansongyee0304@gmail.com>
2022-03-04 13:53:45 -0800

    added Curl

--- bash.js
+++ bash.js
@@ -1,6 +1,7 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
 const cat = require ('./cat');
+const curl = require('./curl')
 
 // Output a prompt
 process.stdout.write('prompt > ');
@@ -17,10 +18,10 @@ process.stdin.on('data', (data) => {
 	}
 	if (cmd.split(' ')[0] === 'cat') {
 		let inputs = cmd.split(' ');
-		// console.log(inputs);
-		// if (inputs[0] === 'cat'){
-			console.log(cmd);
-			cat(inputs[1]);
-		}
+		cat(inputs[1]);
 	}
-);
+	if (cmd.split(' ')[0] === 'curl') {
+		let inputs = cmd.split(' ')
+		curl(inputs[1])
+	}
+});
